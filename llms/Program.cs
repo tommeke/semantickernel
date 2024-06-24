b6f869157abe36cbf609a4e8a5cae848b6092faa
tommeke <2158381+tommeke@users.noreply.github.com>
2024-06-24 03:52:41 +0200

    initial import

---
+++ llms/Program.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable SKEXP0001, SKEXP0003, SKEXP0010, SKEXP0011, SKEXP0050, SKEXP0052
+
+using System.Diagnostics;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+// Create kernel with a custom http address
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion("phi3", endpoint: new Uri("http://127.0.0.1:11434"), "apikey");
+var kernel = builder.Build();
+
+string prompt = "Write a joke about kittens";
+var response = await kernel.InvokePromptAsync(prompt);
+Console.WriteLine("RPI Llama3 Response: " + response.GetValue<string>());
